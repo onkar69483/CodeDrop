ab560c570728432368ff91af986716123ca363d8
onkar69483 <onkar69483@gmail.com>
2024-07-27 23:21:00 +0530

    Updated svelte.config.js to work for vercel -- changed to adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
