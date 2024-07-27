1b4a3f5d3e5f32a049462704156ad5d829f7ec9c
onkar69483 <onkar69483@gmail.com>
2024-07-28 00:18:05 +0530

    Switch back to adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
